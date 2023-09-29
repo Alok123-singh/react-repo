11aa372862a58cd2b1824949754d84914c75fda6
alok123-singh <aloksinghbais02@gmail.com>
2023-09-29 10:55:28 +0530

    created currency convertor

---
+++ 06currencyConvertor/src/App.jsx
@@ -0,0 +1,106 @@
+import { useCallback, useEffect, useState } from 'react';
+import './App.css'
+import InputBox from './components/InputBox'
+
+function fetchCurrencyOptions(currency){
+  const [data,setData] = useState({});
+  const fetchOptions = useCallback(() => {
+    fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+    .then((response) => response.json())
+    .then((response) => setData(response[currency]))
+    .catch((e) => (console.log(e)))
+    // console.log(data);
+  },[currency]);
+
+  useEffect(fetchOptions,[currency]);
+
+  return (data);
+}
+
+function App() {
+
+  const [currencyName,setCurrencyName] = useState("usd");
+  const [amount,setAmount] = useState();
+  const [transferCurrencyName,setTransferCurrencyName] = useState("inr");
+  const [transferedAmount,setTransferedAmount] = useState();
+
+  const currencyOptions = fetchCurrencyOptions(currencyName);
+  console.log(currencyOptions);
+  
+  const keys = Object.keys(currencyOptions);
+  // console.log(keys);
+
+  const showConvertedAmount = () => {
+    const temp = amount * currencyOptions[transferCurrencyName];
+    setTransferedAmount(temp);
+  };
+
+  const swap = () => {
+    setAmount(transferedAmount);
+    setTransferedAmount(amount);
+    setCurrencyName(transferCurrencyName);
+    setTransferCurrencyName(currencyName);
+  };
+
+  return (
+    <div className='... w-screen h-screen flex justify-center items-center container'>
+
+      <div className='... text-white outerBoxSize flex flex-col flex-wrap justify-center items-center rounded-xl'>
+        <form onSubmit={(e)=>{
+          e.preventDefault();
+        }} className='... flex flex-col justify-center items-center'>
+
+            <div className='... flex justify-center items-center'>
+              <InputBox 
+              label="From" 
+              availaibleOptions={keys} 
+              currentCurrencyName={currencyName} 
+              onCurrencyChange={(changedCurrency)=> setCurrencyName(changedCurrency)}
+              isDisabled = {false}
+              amount = {amount}
+              onAmountChange={(amount) => {
+                if(amount < 0){
+                  alert('Please enter valid amount !');
+                  amount = 0;
+                }
+                else{
+                  setAmount(amount);
+                }
+              }}
+              />
+            </div>
+
+            <div className='... flex justify-center items-center bg-blue-600 rounded-lg w-20 h-10 mt-10 mb-10'>
+              <button 
+              type="button" 
+              className='... text-white'
+              onClick={swap}
+              >SWAP</button>
+            </div>
+
+            <div className='... flex justify-center items-center'>
+              <InputBox 
+              label="To" 
+              availaibleOptions={keys} 
+              currentCurrencyName={transferCurrencyName} 
+              onCurrencyChange={(changedCurrency)=> setTransferCurrencyName(changedCurrency)}
+              isDisabled = {true}
+              amount = {transferedAmount}
+              onAmountChange={() => {}}
+              />
+            </div>
+          
+            <button 
+            type="button" 
+            className='... w-[31rem] h-[3rem] mt-10 bg-blue-600 text-center rounded-lg'
+            onClick={showConvertedAmount}
+            >Convert {currencyName.toUpperCase()} to {transferCurrencyName.toUpperCase()}</button>
+
+        </form>
+      </div>
+    
+    </div>
+  )
+}
+
+export default App
---
+++ 06currencyConvertor/src/components/InputBox.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import '../App.css'
+
+function InputBox({
+    label,
+    availaibleOptions = [],
+    currentCurrencyName,
+    onCurrencyChange,
+    isDisabled = false,
+    amount,
+    onAmountChange
+}) {
+    console.log(availaibleOptions);
+    return (
+        <div className='... flex justify-between items-center'>
+            <div className='... flex justify-center items-center flex-col'>
+
+                <label 
+                className='... bg-blue-600 paraSize rounded-lg flex justify-center items-center mb-2 font-mono' style={{cursor: "default"}}
+                
+                > {label} </label>
+
+                <input 
+                type="number" 
+                className='... rounded-lg w-80 text-black text-center outline-black' 
+                style={{backgroundColor: "#9c9af5",border: "1px solid",cursor: "pointer"}}
+                disabled = {isDisabled}
+                value={amount}
+                onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+                />
+
+            </div>
+            
+            <div className='... flex justify-center items-center flex-col ml-12'>
+
+                <p 
+                className='... bg-blue-600 paraSize rounded-lg flex justify-center items-center mb-2' 
+                style={{cursor: "default"}}
+                
+                > Currency Type </p>
+
+                <select 
+                className='... rounded-lg w-20 text-black' 
+                style={{backgroundColor: "#9c9af5",border: "1px solid",cursor: "pointer"}}
+                value={currentCurrencyName}
+                onChange={(e)=> onCurrencyChange && onCurrencyChange(e.target.value) }
+                >
+
+                    {availaibleOptions.map((currencyName) => (
+
+                        <option key={currencyName} value={currencyName}>
+                            {currencyName}
+                        </option>
+
+                    ))}
+
+                </select>
+
+            </div>
+        
+        </div>
+    )
+}
+
+export default InputBox;
