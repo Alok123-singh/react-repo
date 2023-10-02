04df110b0b9f7dc28dc03551d3ba2fb4a11110fc
alok123-singh <aloksinghbais02@gmail.com>
2023-10-02 16:16:33 +0530

    updated currency converter

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useState } from 'react';
 import './App.css'
 import InputBox from './components/InputBox'
 
+// custom hook
 function fetchCurrencyOptions(currency){
   const [data,setData] = useState({});
   const fetchOptions = useCallback(() => {
